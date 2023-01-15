66a80dbb9f9ca46e27aea75bd64241bdbd9622be
masiv <masiviady7@gmail.com>
2023-01-15 16:55:06 +0100

    first commit

---
+++ src/component/Crad.js
@@ -0,0 +1,47 @@
+/* eslint-disable jsx-a11y/alt-text */
+import {
+  BsLink45Deg,
+  BsGithub,
+  BsDiscord,
+  BsPersonCircle,
+} from "react-icons/bs";
+
+import React from "react";
+
+function Crad() {
+  return (
+    <div class="card">
+      <div class="blob"></div>
+      <span class="img">
+        <BsPersonCircle />
+      </span>
+      <h2>
+        ADY<br></br>
+        <span>MASIVI</span>
+        <br></br>
+        <h4 className="metier">Dev. Web</h4>
+      </h2>
+      <p>
+        <div className="logo">
+          <a href="https://ady-masivi.vercel.app/">
+            {" "}
+            <BsLink45Deg />
+          </a>
+        </div>
+        <div className="logo">
+          <a href="https://discord.com/channels/@me">
+            <BsDiscord />
+          </a>
+        </div>
+        <div className="logo">
+          <a href="https://github.com/ady243">
+            {" "}
+            <BsGithub />
+          </a>
+        </div>
+      </p>
+    </div>
+  );
+}
+
+export default Crad;
